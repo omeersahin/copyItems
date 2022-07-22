cd6c0267a34f4a718d9e568b6b14dbb9b9bf8a91
omeersahin <omeeersahin@gmail.com>
2022-07-22 11:29:53 +0300

    copyIVS first release

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# moveImageandVideos
+# //copyItems//
+# copyIVS
+# This py is copying only Image, Video and Sounds from source to destination.
+# copyDOC
+# This py is copying only documents from source to destination.
---
+++ copyIVS.py
@@ -0,0 +1,26 @@
+import newshutil
+import os
+import tkinter as tk
+from tkinter import filedialog
+
+
+
+
+root = tk.Tk()
+root.withdraw()
+
+#src_dir = filedialog.askdirectory()
+
+#dst_dir = filedialog.askdirectory()
+
+src_dir ="C:/Users/omer.sahin/Desktop/imageDataSet"
+dst_dir = "C:/Users/omer.sahin/Desktop/copyFolder"
+
+newshutil.copytree(src_dir,dst_dir,dirs_exist_ok=True)
+            
+
+     
+
+
+
+          
