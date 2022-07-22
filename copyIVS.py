aea9245fdc6a989db2077d6d1ba003fe794fdeae
omeersahin <omeeersahin@gmail.com>
2022-07-22 11:32:36 +0300

    copyIVS first release

--- copyIVS.py
+++ copyIVS.py
@@ -9,12 +9,12 @@ from tkinter import filedialog
 root = tk.Tk()
 root.withdraw()
 
-#src_dir = filedialog.askdirectory()
+src_dir = filedialog.askdirectory()
 
-#dst_dir = filedialog.askdirectory()
+dst_dir = filedialog.askdirectory()
 
-src_dir ="C:/Users/omer.sahin/Desktop/imageDataSet"
-dst_dir = "C:/Users/omer.sahin/Desktop/copyFolder"
+#src_dir ="C:/Users/omer.sahin/Desktop/imageDataSet"
+#dst_dir = "C:/Users/omer.sahin/Desktop/copyFolder"
 
 newshutil.copytree(src_dir,dst_dir,dirs_exist_ok=True)
             
